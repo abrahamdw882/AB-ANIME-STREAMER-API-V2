0fe0b75e9a4ec6cec1134f761d780bc2411ca16d
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-12-09 12:53:05 -0800

    Update gogo.js

--- src/gogo.js
+++ src/gogo.js
@@ -7,7 +7,7 @@ import {
   } from "./gogo_extractor.js";
  import * as cheerio from "cheerio";
 
-  const BaseURL = ["http://gogoanime.org.za"]; 
+  const BaseURL = ["https://gogoanime.me.in"]; 
   const USER_AGENT =
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36";
   
